15012c7f4c0ea4c890f36f35b0365e60dbe41bc9
eaglesakura <eagle.sakura@gmail.com>
2016-08-02 13:44:33 +0900

    UnitTestのRunnerを更新

--- src/test/java/com/eaglesakura/android/glkit/UnitTestCase.java
+++ src/test/java/com/eaglesakura/android/glkit/UnitTestCase.java
@@ -6,10 +6,10 @@ import com.eaglesakura.android.egl.BuildConfig;
 
 import org.junit.runner.RunWith;
 
-import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.RobolectricTestRunner;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, packageName = BuildConfig.APPLICATION_ID, sdk = 21)
 public abstract class UnitTestCase extends AndroidSupportTestCase {
 }
